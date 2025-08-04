f30d5ec8f4c1a39e72b4da3b66ab8d90bc27a722
ronnft <ronkrisft@gmail.com>
2025-08-05 02:58:44 +0800

    testing 2 divs

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import './App.css'
 
 function App() {
   return (
+    <>
     <div>
       <p>Welcome to my portfolio!</p>
     </div>
+    <div>
+      <p>Welcome to my portfolio!</p>
+    </div>
+    </>
   )
 }
 
